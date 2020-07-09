14253badb43df0f9bbabe63a7a16f39cf2c1b0f4
Abdullah <byzkrkbyzkrkmail.com>
2020-07-09 18:59:02 +0200

    Python Dersinin 9 numaralı Assigment i için oluşturulmuştur.

---
+++ Assignment_9.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name:").title()
+my_name = "Abdullah"
+if name == my_name:
+    print("Hello, {}! The password is: W@12".format(my_name))
+else:
+    print("Hello, {}! See you later.".format(name))
